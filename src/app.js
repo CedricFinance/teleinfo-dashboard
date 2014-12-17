db5109ff23a9b49d71f6c43d831074e6781d7c25
Cédric Finance <cedric.finance@finandric.com>
2014-12-17 08:29:11 +0100

    Add socket.io

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(serveStatic('resources', {'index': ['index.html'] }));
 
-var server = app.listen(3000, function () {
+var metrics = { watts: 340, amperes: 1, heurescreuses: 10000000, heurespleines: 50000000 };
+
+io.on('connection', function (socket) {
+  function publishMetrics() {
+    socket.emit('metrics', metrics);
+    metrics.heurespleines++;
+  }
+  var timer = setInterval(publishMetrics, 1000);
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('disconnect', function(socket) {
+    console.log("disconnect",arguments);
+    clearInterval(timer);
+  })
+});
+
+server.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
   console.log('Listening at http://%s:%s', host, port)
