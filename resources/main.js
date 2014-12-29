01678893c747475ce6a8cadfacb1b08085d35ffc
Cédric Finance <cedric.finance@finandric.com>
2014-12-29 11:25:16 +0100

    Use a FixedQueue for recent values

--- resources/main.js
+++ resources/main.js
@@ -2,9 +2,23 @@ var app = angular.module('electricity-dashboard', {});
 
 app.controller("LiveMetricsCtrl", LiveMetricsCtrl);
 
+var maxLength = 40;
+
 function LiveMetricsCtrl($scope) {
   this.live = { price_period: "HP.." };
   var ctrl = this;
+
+  var previousHP = new FixedQueue(maxLength);
+  var previousHC = new FixedQueue(maxLength);
+  var previousAmps = new FixedQueue(maxLength);
+  var previousWatts = new FixedQueue(maxLength);
+
+  function updateGraphs() {
+    displayValues(previousHP, '#hp_graph', "Wh");
+    displayValues(previousHC, '#hc_graph', "Wh");
+    displayValues(previousAmps, '#amps_graph', "A");
+    displayValues(previousWatts, '#watts_graph', "W");
+  }
 
   var socket = io.connect('http://localhost:3000');
   socket.on('metrics', function (data) {
@@ -12,10 +26,10 @@ function LiveMetricsCtrl($scope) {
       ctrl.live = data;
     })
 
-    push(previousHP, data.peak_hours_index);
-    push(previousHC, data.offpeak_hours_index);
-    push(previousAmps, data.amperes);
-    push(previousWatts, data.watts);
+    previousHP.push(data.peak_hours_index);
+    previousHC.push(data.offpeak_hours_index);
+    previousAmps.push(data.amperes);
+    previousWatts.push(data.watts);
 
     updateGraphs();
 
@@ -33,24 +47,4 @@ LiveMetricsCtrl.prototype.getPricingPeriodLabel = function() {
 
 function displayValues(values, elementSelector, unit) {
   $(elementSelector).sparkline(values, { width: values.length * 2, tooltipSuffix: unit });
-}
-
-var maxLength = 40;
-var previousHP = [];
-var previousHC = [];
-var previousAmps = [];
-var previousWatts = [];
-
-function updateGraphs() {
-  displayValues(previousHP, '#hp_graph', "Wh");
-  displayValues(previousHC, '#hc_graph', "Wh");
-  displayValues(previousAmps, '#amps_graph', "A");
-  displayValues(previousWatts, '#watts_graph', "W");
-}
-
-function push(array, value) {
-  array.push(value);
-  if (array.length > maxLength) {
-    array.splice(0, 1);
-  }
 }
