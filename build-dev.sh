af03decc8335dd591102803c271b5bcdf9805bba
Cédric Finance <cedric.finance@finandric.com>
2015-01-04 12:15:59 +0100

    Use node-runtime-dev image for development

--- build-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t teleinfo-dashboard-dev - <Dockerfile-dev
--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --rm -p 3000:3000 --link teleinfo-activemq:mq -v $PWD:/home/app teleinfo-dashboard-dev
+docker run -it --rm -p 3000:3000 --link teleinfo-activemq:mq -e ACTIVEMQ_USER=subscriber -e ACTIVEMQ_PASSWORD=subscriber -v $PWD:/home/app node-runtime-dev
