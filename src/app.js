2d43238b8434f7446b15ed94514b205624efb87a
Cédric Finance <cedric.finance@finandric.com>
2015-01-11 23:09:45 +0100

    Add a service to get latest indexes from mongodb

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var serveStatic = require('serve-static');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var Q = require('q');
 var config = require('./config');
 var activemq_connect = require('./activemq_connect');
 
@@ -25,6 +26,26 @@ activemq_connect(config.activemq).then(function(client) {
 });
 
 app.use(serveStatic('resources', {'index': ['index.html'] }));
+
+var MongoClient = require('mongodb').MongoClient;
+var connect = Q.denodeify(MongoClient.connect);
+connect(config.mongo.url).then(function(db) {
+  console.log("Connected to mongodb");
+
+  var collection = db.collection('raw_metrics');
+
+  app.get("/api/v1/indexes", function(req, res) {
+    collection.find({}).sort({ date: -1 }).limit(100).toArray(function(err, docs) {
+      var results = [ [ "dates", "Heures Creuses" , "Heures Pleines" ] ];
+      docs.forEach(function(raw_metric) {
+        results.push([ raw_metric.date, raw_metric.offpeak_hours_index , raw_metric.peak_hours_index ]);
+      });
+      res.json(results);
+    });
+
+  });
+
+}).done();
 
 server.listen(3000, function () {
   var host = server.address().address
